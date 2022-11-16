fb8be3bae3a232e8c3678375f6ba110804229b66
Julia <kico1.14.5@gmail.com>
2022-11-16 16:11:01 -0500

    Spawn + list of ports to go to

--- bot.py
+++ bot.py
@@ -29,26 +29,33 @@ class Bot:
             return Spawn(tick.map.ports[closest_ports[0][1]])
         
         # Plan the next ports
-        if len(Bot.ports_plan) == 0:
-            
+        if len(Bot.ports_plan) == 1:
             current = 0
             while current < len(Bot.ports_plan):
                 distance = []
                 for poss_next_port in range(len(tick.map.ports)):
-                    if current == poss_next_port:
+                    if poss_next_port in Bot.ports_plan:
                         continue
                     
-                    coord = [tick.map.ports[Bot.ports_plan[poss_next_port]].row-tick.map.ports[current].row,
-                            tick.map.ports[Bot.ports_plan[poss_next_port]].column-tick.map.ports[Bot.ports_plan[current]].column]
+                    # Use .row instead of [0]
+                    coord = [tick.map.ports[Bot.ports_plan[current]].row - tick.map.ports[poss_next_port].row,
+                            tick.map.ports[Bot.ports_plan[current]].column - tick.map.ports[poss_next_port].column]
+                    print(coord)
                     distance_current_port = math.sqrt(coord[0]**2 + coord[1]**2)
 
-                    if len(distance) == 0:
-                        distance.append([distance_current_port, poss_next_port])
-                    elif distance_current_port < distance:
-                        distance[0] = [distance_current_port, poss_next_port]
-                Bot.ports_plan.append(distance[0][1])
-
+                    distance.append([distance_current_port, poss_next_port])
+                    
+                    distance = sorted(distance)
+                
                 current += 1
+
+                if current == len(tick.map.ports)-1:
+                    Bot.ports_plan.append(current)
+                    Bot.ports_plan.append(Bot.ports_plan[0])
+                    break
+                else:
+                    #print(distance)
+                    Bot.ports_plan.append(distance[0][1])
             
         if tick.currentLocation == tick.map.ports[Bot.ports_plan[0]] and len(tick.visitedPortIndices) == 0:
             return Dock()
