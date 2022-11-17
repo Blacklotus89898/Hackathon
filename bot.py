fa5ef369da874295df4088a3e1cf0396b8c3bc12
Julia <kico1.14.5@gmail.com>
2022-11-17 16:18:37 -0500

    Final bot

--- bot.py
+++ bot.py
@@ -13,6 +13,107 @@ class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
 
+    def check_obstacle(list_topology, direction, max):
+        new_direction = direction
+        if direction == "SE":
+            if list_topology[2][2] < max:
+                new_direction = direction
+            else:
+                if list_topology[1][2] < max:
+                    new_direction = "E"
+                elif list_topology[2][1] < max:
+                    new_direction = "S"
+                elif list_topology[0][2] < max:
+                    new_direction = "NE"
+                elif list_topology[2][0] < max:
+                    new_direction = "SW"
+                elif list_topology[0][1] < max:
+                    new_direction = "N"
+                elif list_topology[1][0] < max:
+                    new_direction = "W"
+        elif direction == "SW":
+            if list_topology[2][0] < max:
+                new_direction = direction
+            else:
+                if list_topology[1][0] < max:
+                    new_direction = "W"
+                elif list_topology[2][1] < max:
+                    new_direction = "S"
+                elif list_topology[2][2] < max:
+                    new_direction = "SE"
+                elif list_topology[1][2] < max:
+                    new_direction = "NW"
+                elif list_topology[0][1] < max:
+                    new_direction = "N"
+                elif list_topology[0][1] < max:
+                    new_direction = "E"
+        elif direction == "S":
+            if list_topology[2][1] < max:
+                new_direction = direction
+            else:
+                if list_topology[2][2] < max:
+                    new_direction = "SE"
+                elif list_topology[2][0] < max:
+                    new_direction = "SW"
+                elif list_topology[1][2] < max:
+                    new_direction = "E"
+                elif list_topology[1][0] < max:
+                    new_direction = "W"
+                elif list_topology[0][2] < max:
+                    new_direction = "NE"
+                elif list_topology[0][0] < max:
+                    new_direction = "NW"
+        elif direction == "NE":
+            if list_topology[0][2] < max:
+                new_direction = direction
+            else:
+                if list_topology[0][1] < max:
+                    new_direction = "N"
+                elif list_topology[1][2] < max:
+                    new_direction = "E"
+                elif list_topology[2][2] < max:
+                    new_direction = "SE"
+                elif list_topology[0][0] < max:
+                    new_direction = "NW"
+                elif list_topology[2][1] < max:
+                    new_direction = "S"
+                elif list_topology[1][0] < max:
+                    new_direction = "W"
+        elif direction == "NW":
+            if list_topology[0][0] < max:
+                new_direction = direction
+            else:
+                if list_topology[0][1] < max:
+                    new_direction = "N"
+                elif list_topology[1][0] < max:
+                    new_direction = "W"
+                elif list_topology[0][2] < max:
+                    new_direction = "NE"
+                elif list_topology[2][0] < max:
+                    new_direction = "SW"
+                elif list_topology[1][2] < max:
+                    new_direction = "E"
+                elif list_topology[2][1] < max:
+                    new_direction = "S"
+        elif direction == "N":
+            if list_topology[0][1] < max:
+                new_direction = direction
+            else:
+                if list_topology[0][0] < max:
+                    new_direction = "NW"
+                elif list_topology[0][2] < max:
+                    new_direction = "NE"
+                elif list_topology[1][0] < max:
+                    new_direction = "W"
+                elif list_topology[1][2] < max:
+                    new_direction = "E"
+                elif list_topology[2][0] < max:
+                    new_direction = "SW"
+                elif list_topology[2][2] < max:
+                    new_direction = "SE"
+
+        return new_direction
+
         
     def get_next_move(self, tick: Tick) -> Action:
         """
@@ -70,30 +171,32 @@ class Bot:
         col = tick.map.ports[Bot.ports_plan[Bot.i]].column - tick.currentLocation.column
         row = tick.map.ports[Bot.ports_plan[Bot.i]].row - tick.currentLocation.row
         
-       
+        topology_around =  [[tick.map.topology[tick.currentLocation.row-1][tick.currentLocation.column-1],tick.map.topology[tick.currentLocation.row-1][tick.currentLocation.column],tick.map.topology[tick.currentLocation.row-1][tick.currentLocation.column+1]],
+                            [tick.map.topology[tick.currentLocation.row][tick.currentLocation.column-1],tick.map.topology[tick.currentLocation.row][tick.currentLocation.column],tick.map.topology[tick.currentLocation.row][tick.currentLocation.column+1]],
+                            [tick.map.topology[tick.currentLocation.row+1][tick.currentLocation.column-1],tick.map.topology[tick.currentLocation.row+1][tick.currentLocation.column],tick.map.topology[tick.currentLocation.row+1][tick.currentLocation.column+1]]]
         
+        max_tide = tick.map.tideLevels.max
+
+
         if row > 0:
             if col > 0:
-                Bot.return_plan.append(Sail("SE"))
-                return Sail("SE")
+                return Sail(Bot.check_obstacle(topology_around, "SE", max_tide))
             elif col < 0:
-                Bot.return_plan.append(Sail("SW"))
-                return Sail("SW")
+                return Sail(Bot.check_obstacle(topology_around, "SW", max_tide))
             else:
-                Bot.return_plan.append(Sail("S"))
-                return Sail("S")
+                return Sail(Bot.check_obstacle(topology_around, "S", max_tide))
         elif row < 0: 
             if col > 0: 
-                return Sail("NE")
+                return Sail(Bot.check_obstacle(topology_around, "NE", max_tide))
             elif col < 0:
-                return Sail("NW")
+                return Sail(Bot.check_obstacle(topology_around, "NW", max_tide))
             else:
-                return Sail("N")
+                return Sail(Bot.check_obstacle(topology_around, "N", max_tide))
         else:
             if col > 0 :
-                return Sail("E")
+                return Sail(Bot.check_obstacle(topology_around, "E", max_tide))
             elif col < 0:
-                return Sail("W")
+                return Sail(Bot.check_obstacle(topology_around, "W", max_tide))
             else:
                 if Bot.i != len(Bot.ports_plan)-1:
                     Bot.i += 1
