cc003bf831e79120cc60b9a1c484b21cb64d04ef
Blacklotus89898 <song.yang.chen89898@gmail.com>
2022-11-17 11:47:31 -0500

    gud one

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import math
 class Bot:
     
     ports_plan = []
+    
+    i = 0
+
+    return_plan = []
 
     def __init__(self):
         print("Initializing your super mega duper bot")
@@ -40,7 +44,7 @@ class Bot:
                     # Use .row instead of [0]
                     coord = [tick.map.ports[Bot.ports_plan[current]].row - tick.map.ports[poss_next_port].row,
                             tick.map.ports[Bot.ports_plan[current]].column - tick.map.ports[poss_next_port].column]
-                    print(coord)
+
                     distance_current_port = math.sqrt(coord[0]**2 + coord[1]**2)
 
                     distance.append([distance_current_port, poss_next_port])
@@ -57,7 +61,42 @@ class Bot:
                     #print(distance)
                     Bot.ports_plan.append(distance[0][1])
             
-        if tick.currentLocation == tick.map.ports[Bot.ports_plan[0]] and len(tick.visitedPortIndices) == 0:
-            return Dock()
+   
         
-        return Sail(directions[tick.currentTick % len(directions)])
+        
+        #return Sail(directions[tick.currentTick % len(directions)])
+        #for i in range(1,len(ports_plan)):
+        
+        col = tick.map.ports[Bot.ports_plan[Bot.i]].column - tick.currentLocation.column
+        row = tick.map.ports[Bot.ports_plan[Bot.i]].row - tick.currentLocation.row
+        
+       
+        
+        if row > 0:
+            if col > 0:
+                Bot.return_plan.append(Sail("SE"))
+                return Sail("SE")
+            elif col < 0:
+                Bot.return_plan.append(Sail("SW"))
+                return Sail("SW")
+            else:
+                Bot.return_plan.append(Sail("S"))
+                return Sail("S")
+        elif row < 0: 
+            if col > 0: 
+                return Sail("NE")
+            elif col < 0:
+                return Sail("NW")
+            else:
+                return Sail("N")
+        else:
+            if col > 0 :
+                return Sail("E")
+            elif col < 0:
+                return Sail("W")
+            else:
+                if Bot.i != len(Bot.ports_plan)-1:
+                    Bot.i += 1
+                print(tick.map.ports[Bot.ports_plan[Bot.i]].column )
+                print(tick.map.ports[Bot.ports_plan[Bot.i]].row) 
+                return Dock()
